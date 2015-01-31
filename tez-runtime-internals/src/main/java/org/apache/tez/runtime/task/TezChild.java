8d828457aefa742d217b90c21509bbdc9bde1129
Siddharth Seth <sseth@apache.org>
2015-01-30 16:02:32 -0800

    TEZ-2019. Temporarily allow the scheduler and launcher to be specified via configuration. (sseth)

--- tez-runtime-internals/src/main/java/org/apache/tez/runtime/task/TezChild.java
+++ tez-runtime-internals/src/main/java/org/apache/tez/runtime/task/TezChild.java
@@ -362,7 +362,8 @@ public class TezChild {
       }
       if (!isLocal) {
         RPC.stopProxy(umbilical);
-        LogManager.shutdown();
+        // TODO Temporary change. Revert. Ideally, move this over to the main method in TezChild if possible.
+//        LogManager.shutdown();
       }
     }
   }
