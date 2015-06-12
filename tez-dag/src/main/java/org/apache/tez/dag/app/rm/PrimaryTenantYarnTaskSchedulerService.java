09d81c2ce689cda0938749f23030f6aafdfd296a
yunqi <t-yunzha@microsoft.com>
2015-06-12 14:43:20 -0700

    hmm, nothing really

---
+++ tez-dag/src/main/java/org/apache/tez/dag/app/rm/PrimaryTenantYarnTaskSchedulerService.java
@@ -0,0 +1,28 @@
+/**
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.tez.dag.app.rm;
+
+/**
+ * TODO: Start with an extension on top of the regular YARN task scheduler,
+ * and we might need to change this to a completely different class later
+ * if we want to do much fancier stuff here
+ */
+public class PrimaryTenantYarnTaskSchedulerService extends 
+                 YarnTaskSchedulerService {
+}
